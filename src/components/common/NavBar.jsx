05451aea10d32e2edbb8d2e252504bcc67ebb1f3
HaroldF415 <harold.f415@gmail.com>
2023-05-21 05:07:18 -0400

    MobileNav will be deprecated soon. Using Collapse instead

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,19 +1,12 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Navbar, MobileNav, Typography, IconButton } from "@material-tailwind/react";
+import { Navbar, Collapse, Typography, IconButton } from "@material-tailwind/react";
 
-const CloseSVG = () => {
+// eslint-disable-next-line react/prop-types
+const IconSVG = ({ shape }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" className="h-6 w-6" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-    </svg>
-  );
-};
-
-const HamburgerSVG = () => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" stroke="currentColor" strokeWidth={2}>
-      <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+      {shape === "close" ? <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" /> : shape === "hamburger" ? <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" /> : null}
     </svg>
   );
 };
@@ -55,22 +48,22 @@ const Navigation = () => {
     <>
       <Navbar className="sticky inset-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4">
         <div className="flex items-center justify-between text-blue-gray-900">
-          <Typography as="a" href="#" className="mr-4 cursor-pointer py-1.5 font-medium">
-            VitaTrack
+          <Typography variant="small" className="mr-4 cursor-pointer py-1.5 font-medium">
+            <Link to="/"> VitaTrack</Link>
           </Typography>
           <div className="flex items-center gap-4">
             <div className="mr-4 hidden lg:block">
               <NavLinkList />
             </div>
             <IconButton variant="text" className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden" ripple={false} onClick={() => setOpenNav(!openNav)}>
-              {openNav ? <CloseSVG /> : <HamburgerSVG />}
+              {openNav ? <IconSVG shape="close" /> : <IconSVG shape="hamburger" />}
             </IconButton>
           </div>
         </div>
         {/* Joshua and Keeanu helped me fix this */}
-        <MobileNav className="flex justify-end" open={openNav}>
+        <Collapse className="flex justify-end" open={openNav}>
           <NavLinkList />
-        </MobileNav>
+        </Collapse>
       </Navbar>
     </>
   );
